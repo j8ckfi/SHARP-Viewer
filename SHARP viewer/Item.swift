4e4440bdeec1fab311ff11a6e3cc9955a41a33e7
j8ck <potteryrage@gmail.com>
2025-12-26 13:07:00 -0700

    Initial Commit

---
+++ SHARP viewer/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SHARP viewer
+//
+//  Created by Jack Large on 12/26/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
